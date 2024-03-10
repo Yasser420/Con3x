d7960db04d7d30973420d13d6f4ea4d201e8e9fd
Yasser Hamody <yasserhamody3@gmail.com>
2024-03-11 00:32:55 +0300

    creating navbar

---
+++ ethereum/src/Components/Nav.tsx
@@ -0,0 +1,17 @@
+const Nav: React.FC = () => {
+
+    return (
+        <div>
+        <nav className="bg-gray-800 py-4">
+          <div className="container mx-auto px-4">
+            <div className="flex items-center justify-between">
+              <div className="text-gray-100 font-bold text-4xl">Ethereum</div>
+              <div className="text-orange-600 text-3xl font-extrabold">Con3x</div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+}
+export default Nav;
+
